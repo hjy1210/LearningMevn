454f3042b380b773c41477ffb2a0b6b952410bac
hjyanghj <hjyanghj@gmail.com>
2020-10-22 23:35:15 +0800

    mevn-cli-nuxt: use form in create.vue

--- mevn-cli-nuxt/client/pages/create.vue
+++ mevn-cli-nuxt/client/pages/create.vue
@@ -1,16 +1,43 @@
 <template>
-  <center>
-    <br />
-    <h1>This is the create component</h1>
-  </center>
+  <div>
+    <h1>Create A Post</h1>
+    <form @submit.prevent="addPost">
+      <div>
+        <div>
+          <div>
+            <label>Post Title:</label>
+            <input v-model="post.title" type="text" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <label>Post Body:</label>
+            <textarea v-model="post.body" rows="5"></textarea>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div>
+        <button>Create</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      post: {},
+    }
+  },
+  methods: {
+    addPost() {
+      console.log(this.post)
+    },
   },
 }
 </script>
-
 <style scoped></style>
