4ec923b615c5996b450014e2556c8664963a37db
hjyanghj <hjyanghj@gmail.com>
2020-10-24 00:05:05 +0800

    mevn-cli-nuxt: add _id.vue 練習 Dynamic Page

---
+++ mevn-cli-nuxt/client/pages/_id.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Edit User</h1>
+    <form @submit.prevent="updatePost">
+      <div>
+        <div>
+          <div>
+            <label>User Title:{{ id }}</label>
+            <input v-model="form.name" type="text" />
+          </div>
+          <div>
+            <label>User Age:</label>
+            <input v-model="form.age" type="text" />
+          </div>
+        </div>
+      </div>
+      <br />
+      <div>
+        <button>Update</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+export default {
+  data() {
+    return {
+      form: {},
+      id: this.$route.params.id,
+    }
+  },
+  created() {
+    const uri = `http://localhost:9000/api/${this.id}`
+    axios.put(uri).then((response) => {
+      this.form = response.data
+    })
+  },
+  methods: {
+    updatePost() {
+      const uri = `http://localhost:9000/api/${this.id}`
+      axios.put(uri, this.form).then(() => {
+        // this.$router.push({ name: 'posts' })
+      })
+    },
+  },
+}
+</script>
+<style scoped></style>
