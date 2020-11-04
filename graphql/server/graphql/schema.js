88b7734262722eb36cc0690ae02a359bcec71c0a
hjyanghj <hjyanghj@gmail.com>
2020-11-04 09:49:55 +0800

    graphql: add addChef, updateChef, deleteChef, updateDish, deleteDish

--- graphql/server/graphql/schema.js
+++ graphql/server/graphql/schema.js
@@ -3,8 +3,7 @@ const graphql = require('graphql');
 const Dish = require('./mongo-models/dish');
 const Chef = require('./mongo-models/chef');
 
-
-const path = require('path')
+const path = require('path');
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, buildSchema } = require('graphql');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 // GraphQL schema
@@ -29,46 +28,60 @@ var resolvers = {
 	Query: {
 		message: () => {
 			return 'Yang say Hello world!';
-        },
-        dishes: ()=>{
-           return  Dish.find()
-        },
-        chefs: ()=>{
-            return Chef.find()
-        },
-        chef:(parent,args)=>{
-            return Chef.findById(args.id)
-        },
-        dish:(parent,args)=>{
-            return Dish.findById(args.id)
-        },
-        getDishes:async (parent, args)=>{
-            return await Dish.find({chefid:args.chefid})
-        }
-    },
-    Chef:{
-        dishes:async(parent,args)=>{
-            return await Dish.find({chefid:parent.id})
-        }
-    },
-    Dish:{
-        chef:async(parent,args)=>{
-            return await Chef.findById(parent.chefid)
-        }
-    },
-    Mutation : {
-        addChef:(root,args,context,info) => {
-            let newChef = new Chef({name:args.input.name, rating:args.input.rating});
-            return newChef.save()
-        },
-        addDish:(root,args,context,info) => {
-            let newDish = new Dish({name:args.input.name, country:args.input.country, 
-                tasty:args.input.tasty, chefid:args.input.chefid});
-            return newDish.save()
-        }
-
-    }
-    
+		},
+		dishes: () => {
+			return Dish.find();
+		},
+		chefs: () => {
+			return Chef.find();
+		},
+		chef: (parent, args) => {
+			return Chef.findById(args.id);
+		},
+		dish: (parent, args) => {
+			return Dish.findById(args.id);
+		},
+		getDishes: async (parent, args) => {
+			return await Dish.find({ chefid: args.chefid });
+		}
+	},
+	Chef: {
+		dishes: async (parent, args) => {
+			return await Dish.find({ chefid: parent.id });
+		}
+	},
+	Dish: {
+		chef: async (parent, args) => {
+			return await Chef.findById(parent.chefid);
+		}
+	},
+	Mutation: {
+		addDish: (root, args, context, info) => {
+			let newDish = new Dish({
+				name: args.input.name,
+				country: args.input.country,
+				tasty: args.input.tasty,
+				chefid: args.input.chefid
+			});
+			return newDish.save();
+		},
+		updateDish: async (root, args, context, info) => {
+			return await Dish.findByIdAndUpdate(args.id, args.input, { useFindAndModify: false, new: true });
+		},
+		deleteDish: async (parent, args, context) => {
+			return await (await Dish.findById(args.id)).remove();
+		},
+		addChef: async (root, args, context, info) => {
+			let newChef = new Chef(args.input);
+			return await newChef.save();
+		},
+		updateChef: async (root, args, context, info) => {
+			return await Chef.findByIdAndUpdate(args.id, args.input, { useFindAndModify: false, new: true });
+		},
+		deleteChef: async (parent, args, context) => {
+			return await (await Chef.findById(args.id)).remove();
+		}
+	}
 };
 
 const executableSchema = makeExecutableSchema({
