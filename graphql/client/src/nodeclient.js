fcf2077d138cadee5bb62d9145f3df36915ab837
hjyanghj <hjyanghj@gmail.com>
2020-11-06 08:43:12 +0800

    graphql: client project add nodeclient.js use to inspect graphql api from node

---
+++ graphql/client/src/nodeclient.js
@@ -0,0 +1,15 @@
+const axios = require("axios")
+axios({
+  url: 'http://localhost:9000/graphql',
+  method: 'post',
+  data: {
+    query: `query abcd
+    {
+        form:chef(id:"5fa0022c2903b100d0cc75bb")
+        {id,name,rating,dishes{id,name}}
+    }
+    `
+  }
+}).then((result) => {
+  console.log(result.data)
+});
