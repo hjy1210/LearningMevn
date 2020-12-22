8654a5f3453bcbcd2a6d4698af6de0cdae5734ba
hjyanghj <hjyanghj@gmail.com>
2020-12-22 23:22:52 +0800

    vue-client-hello: add calendar.vue  using astronomia

--- vue-cli-hello/src/App.vue
+++ vue-cli-hello/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <MagicCube />
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MagicCube />-->
+    <calendar />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import MagicCube from './components/MagicCube.vue'
+// import MagicCube from './components/MagicCube.vue'
+import calendar from './components/calendar.vue'
 export default {
   name: 'App',
   components: {
     //HelloWorld
-    MagicCube
+    //MagicCube
+    calendar
   }
 }
 </script>
